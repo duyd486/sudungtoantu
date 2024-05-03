9d424a400b91a0d6aaef36a4dafa12cf43f2f857
duyd486 <duyd486@gmail.com>
2024-05-04 01:40:30 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float width;
+        float height;
+        Console.WriteLine("Nhap chieu dai: ");
+        width = float.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap chieu rong: ");
+        height = float.Parse(Console.ReadLine());
+        float area = width * height;
+        Console.WriteLine("Dien tich hinh chu nhat la: " + area);
+    }
+}
